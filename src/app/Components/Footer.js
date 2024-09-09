0e880b80dd251e7a5533b7e9781c0643ba593abd
Soraya B <sorayaboza@gmail.com>
2024-09-09 09:24:50 -0700

    added navbar and footer components

---
+++ src/app/Components/Footer.js
@@ -0,0 +1,11 @@
+"use client";
+
+import { useState } from "react";
+
+export default function Footer() {
+  return (
+    <header className="bg-[#3D3860]">
+        Footer
+    </header>
+  );
+}
---
+++ src/app/Components/Navbar.js
@@ -0,0 +1,11 @@
+"use client";
+
+import { useState } from "react";
+
+export default function Navbar() {
+  return (
+    <header className="bg-[#3D3860]">
+        Navbar
+    </header>
+  );
+}
