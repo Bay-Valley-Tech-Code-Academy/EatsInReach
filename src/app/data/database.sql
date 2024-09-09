9d69b3d0652a88c5c37e186b8e7114f5c65908a8
Soraya B <sorayaboza@gmail.com>
2024-09-09 10:37:13 -0700

    Edited the data page to be an example. You'll have to edit your .env file to apply the changes for the DB_NAME

--- src/app/data/database.sql
+++ src/app/data/database.sql
@@ -1,19 +1,19 @@
-CREATE DATABASE rural_eats_db;
+CREATE DATABASE eats_in_reach_db;
 
 
 -- Connect to the newly created database
-\c my_database;
+\c eats_in_reach_db;
 
 
 -- Create the table
-CREATE TABLE rural_table(
+CREATE TABLE eats_in_reach_table(
     id SERIAL PRIMARY KEY,
     description TEXT,
     category TEXT
 );
 
 
-INSERT INTO rural_table (description, category)
+INSERT INTO eats_in_reach_table (description, category)
 VALUES
     ('Item 1 description', 'Category A'),
     ('Item 2 description', 'Category B'),
