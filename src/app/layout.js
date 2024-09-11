988a1944800197bf949db54469454e3245b8e40a
Misael Bolainez <misaelbolainez@gmail.com>
2024-09-11 10:52:09 -0700

    feat(login/signup): Finished the login page and connected it to firebase

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,9 +3,7 @@ import "./globals.css";
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="h-screen w-full">
-      <body className="min-h-screen w-full">
-        {children}
-      </body>
+      <body className="min-h-screen w-full">{children}</body>
     </html>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -2,20 +2,23 @@
 
 import Navbar from "./Components/Navbar";
 import Link from "next/link";
+import { Italiana } from "next/font/google";
+
+const italiana = Italiana({
+  subsets: ["latin"],
+  weight: ["400"],
+});
 
 export default function Landing() {
   return (
     <div>
       <Navbar />
-      <div className="w-full flex flex-col items-center">
-        <h1 className="mt-20 text-8xl">Eats in Reach</h1>
-        <h2>Reach for Flavor, Anytime</h2>
-        <div>
-          <Link href="/Pages/LoginUser">
-            <button>User Login</button>
-          </Link>
-          <Link href="/Pages/LoginUser">
-            <button>Vendor Login</button>
+      <div className="w-full flex flex-col items-center landing-page">
+        <h1 className={`mt-20 text-6xl sm:text-8xl md:text-9xl landing-header ${italiana.className}`}>Eats in Reach</h1>
+        <h2 className="text-2xl sm:text-4xl md:text-5xl mt-4">Reach for Flavor, Anytime</h2>
+        <div className="mt-20">
+          <Link href="/Pages/Login">
+            <button className="text-1xl sm:text-2xl md:text-3xl bg-gray-300 p-3 sm:p-4 md:p-5 rounded-full border border-black">Login</button>
           </Link>
         </div>
       </div>
