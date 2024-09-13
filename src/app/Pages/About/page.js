5e7f95db409347db860f4f9ea136fc055c88d7fb
kxtmg <77362684+kxtmg@users.noreply.github.com>
2024-09-13 16:14:37 -0700

    Added Learn More info
    Improved color changes and Learn More info card when clicked

--- src/app/Pages/About/page.js
+++ src/app/Pages/About/page.js
@@ -1,37 +1,91 @@
 "use client"
 
+import { useState } from "react";
 import Link from "next/link";
 import Navbar from "../../Components/Navbar";
 import Footer from "../../Components/Footer";
 import LearnMoreCard from "../../Components/LearnMoreCard";
+import LearnMoreInfoCard from "../../Components/LearnMoreInfoCard";
 import IconMapMarker from "@/Components/IconMapMarker";
 import IconBxDonateHeart from "@/Components/IconBxDonateHeart";
 import IconStars from "@/Components/IconStars";
 
 const cardData = [
     {
-        image: <IconMapMarker/>,
+        icon: <IconMapMarker/>,
         title: "Discover Local Gems",
-        description: "Explore a diverse range of food vendors right from your device. Whether you’re craving a gourmet food truck meal, a home-cooked specialty, or a unique dish from a small, family-owned eatery, we’ve got you covered.",
-        link: "/"
+        description: (
+            <>
+            Explore a diverse range of food vendors right from your device. Whether you’re craving a gourmet food truck meal, 
+            a home-cooked specialty, or a unique dish from a small, family-owned eatery, we’ve got you covered.
+            </>
+        ),
+        info: (
+            <>
+            Dive into a vibrant tapestry of local flavors and hidden treasures right at your fingertips. 
+            Our platform brings the diverse culinary landscape of your community to life, offering you 
+            seamless access to a wide array of food vendors, from trendy food trucks to beloved family-owned eateries. <br/><br/>
+            Whether you’re in the mood for a gourmet meal from a renowned food truck, a comforting dish from a time-honored
+            family recipe, or an adventurous bite from a boutique eatery, you can find it all through our app. Each vendor
+            is carefully curated to ensure you experience the best that your local food scene has to offer.
+            Our easy-to-navigate interface allows you to browse vendor profile and view enticing photos of their dishes. <br/><br/>
+            With detailed vendor information, discovering your next favorite meal has never been more convenient. 
+            From discovering new local hotspots to revisiting old favorites, our platform is your gateway to a richer, more flavorful food adventure.
+            </>
+        )
     },
     {
-        image: <IconBxDonateHeart/>,
+        icon: <IconBxDonateHeart/>,
         title: "Support Small Businesses",
-        description: "Our platform helps local vendors reach a broader audience, giving them the exposure they deserve and helping them thrive. Your support directly contributes to the growth and sustainability of these cherished community businesses.",
-        link: "/"
+        description: (
+            <>
+            Our platform helps local vendors reach a broader audience, giving them the exposure they deserve and helping them thrive. 
+            Your support directly contributes to the growth and sustainability of these cherished community businesses.
+            </>
+        ),
+        info: (
+            <>At the heart of our platform lies a commitment to amplifying the voices of small businesses. By connecting local vendors with a wider audience,
+            we help them thrive and bring unique, quality products and services to your community.<br/><br/>
+            When you choose to shop with small businesses featured on our platform, you’re doing more than just making a purchase; 
+            you’re investing in the growth and sustainability of local entrepreneurs. Our service is designed to enhance their visibility and reach, 
+            offering them the tools they need to showcase their offerings to a broader audience.
+            </>
+        ),
     },
     {
-        image: <IconStars/>,
+        icon: <IconStars/>,
         title: "Access Unique Offerings",
-        description: "Find and enjoy distinctive culinary experiences that you won’t find in mainstream restaurants. Our app features real-time updates, allowing you to locate vendors, view their menus, and get notifications about their special promotions.",
-        link: "../Restaurants"
+        description: (
+            <>
+            Find and enjoy distinctive culinary experiences that you won’t find in mainstream restaurants. Our app features real-time updates,
+            allowing you to locate vendors, view their menus, and get notifications about their special promotions.
+            </>
+        ),
+        info: (
+            <>
+            Unleash your taste buds and explore a world of unique culinary delights that stand out from the mainstream. Our platform is designed
+            to connect you with distinctive food offerings and memorable dining experiences that you won’t find anywhere else.
+            </>
+        ),
     }
   ];
 
 export default function About() {
+
+    const [ learnMoreToggle, setLearnMoreToggle ] = useState(false);
+    const [ selectedCard, setSelectedCard ] = useState(null);
+
+    function handleLearnMoreToggle() {
+        return setLearnMoreToggle((show) => !show);
+    }
+
+    function handleClickedLearnMore(cardIndex) {
+        setSelectedCard(cardIndex);
+        handleLearnMoreToggle();
+    }
     return (
-        <div>
+        <>
+        <div className={learnMoreToggle ? "blur-md" : "blur-none"}>
             <Navbar/>
             <div className="flex flex-col items-center bg-[#AAD15F] p-12">
                 <h1 className="text-3xl text-center font-bold pb-8">
@@ -57,10 +111,10 @@ export default function About() {
                         {cardData.map((card, index) => (
                             <LearnMoreCard
                             key={index}
-                            image={card.image}
+                            icon={card.icon}
                             title={card.title}
                             description={card.description}
-                            link={card.link}
+                            effect={()=>handleClickedLearnMore(card.info)}
                             />
                         ))}
                     </div>
@@ -88,5 +142,11 @@ export default function About() {
                 </div>
                 <Footer/>
         </div>
+        <div className={learnMoreToggle ? "visible" : "hidden"}>
+            <LearnMoreInfoCard
+            effect={handleLearnMoreToggle}
+            info={selectedCard}/>
+        </div>
+        </>
     );
 }
