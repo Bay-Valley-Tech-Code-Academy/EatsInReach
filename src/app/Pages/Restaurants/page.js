7e16fd10c47c7c69d02949a883b5142a5342e9a3
Soraya <60835721+sorayaboza@users.noreply.github.com>
2024-09-16 13:29:10 -0700

    small syntax fixes

--- src/app/Pages/Restaurants/page.js
+++ src/app/Pages/Restaurants/page.js
@@ -99,8 +99,8 @@ export default function RestaurantPage() {
               <svg 
                 xmlns="http://www.w3.org/2000/svg" 
                 width="24" height="24" viewBox="0 0 24 24" 
-                fill="none" stroke="#000000" stroke-width="2" 
-                stroke-linecap="round" stroke-linejoin="round">
+                fill="none" stroke="#000000" strokeWidth="2" 
+                strokeLinecap="round" strokeLinejoin="round">
                 <line 
                   x1="4" y1="21" x2="4" y2="14">
                 </line>
