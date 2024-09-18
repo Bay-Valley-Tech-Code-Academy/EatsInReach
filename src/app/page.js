617e2c31529c6d3517ac4670bb3c1d611902a343
Javier <javier.miranda4420@gmail.com>
2024-09-18 16:32:40 -0700

    moved text on landing page to the top of the page

--- src/app/page.js
+++ src/app/page.js
@@ -22,58 +22,56 @@ export default function Landing() {
     { name: "Sushi Central", image_url: "sushi-1.jpg" },
     { name: "Burger Bonanza", image_url: "burger-1.jpg" },
     { name: "Taco Town", image_url: "taco-1.jpg" },
-    { name: "Steak House", image_url: "steak-1.jpg" }
+    { name: "Steak House", image_url: "steak-1.jpg" },
   ];
 
   return (
-    <div className ="">
-     <Navbar />
-<div className="w-full flex flex-col items-center landing-page justify-start">
-  <h1
-    className={`mt-2 sm:mt-20 md:mt-24 p-4 text-6xl sm:text-8xl md:text-9xl landing-header ${italiana.className}`}
-  >
-    Eats in Reach
-  </h1>
-  <h2 className="text-2xl sm:text-4xl md:text-5xl">
-    Reach for Flavor, Anytime
-  </h2>
-  {!currentUser && (
-    <div className="mt-12 sm:mt-16 md:mt-20">
-      <Link href="/Pages/Login">
-        <button className="text-1xl sm:text-2xl md:text-3xl bg-[#AAD15F] hover:bg-[#8A9C4C] p-3 sm:p-4 md:p-5 rounded-full border border-black">
-          Login
-        </button>
-      </Link>
-    </div>
-  )}
-</div>
-
+    <div className="">
+      <Navbar />
+      <div className="w-full flex flex-col items-center landing-page">
+        <h1
+          className={`p-4 text-6xl sm:text-8xl md:text-9xl landing-header ${italiana.className} mt-4`}
+        >
+          Eats in Reach
+        </h1>
+        <h2 className="text-2xl sm:text-4xl md:text-5xl">
+          Reach for Flavor, Anytime
+        </h2>
+        {!currentUser && (
+          <div className="mt-12 sm:mt-16 md:mt-20">
+            <Link href="/Pages/Login">
+              <button className="text-1xl sm:text-2xl md:text-3xl bg-[#AAD15F] hover:bg-[#8A9C4C] p-3 sm:p-4 md:p-5 rounded-full border border-black">
+                Login
+              </button>
+            </Link>
+          </div>
+        )}
+      </div>
 
       <div className=" flex flex-col min-h-[calc(100vh-120px)] items-center p-4  ">
-  <h3 className="text-2xl text-black bg-Cream mt-4 mb-3 p-1 rounded-4xl hover:underline hover:-translate-y-1">Our Favorites</h3>
+        <h3 className="text-2xl text-black bg-Cream mt-4 mb-3 p-1 rounded-4xl hover:underline hover:-translate-y-1">
+          Our Favorites
+        </h3>
 
-  {/* Displaying restaurants dynamically */}
-  <div className="bg-Cream grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 rounded-3xl gap-5 mt-4 scrollbar-hidden overflow-y-auto">
-    {restaurants.slice(0, 5).map((restaurant, index) => (
-      <div
-        key={index}
-        className=" h-64 w-48 p-4 rounded-2xl "
-      >
-        <img
-          src={`/images/${restaurant.image_url}`}
-          alt={`Image of ${restaurant.name}`}
-          className=" w-full h-40 object-cover rounded-xl cursor-pointer"
-          loading="lazy"
-        />
-        <p className="w-full text-center bg-Yellow-Green mt-2 p-1 rounded2xl hover:bg-Yellow-Green">{restaurant.name}</p>
+        {/* Displaying restaurants dynamically */}
+        <div className="bg-Cream grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 rounded-3xl gap-5 mt-4 scrollbar-hidden overflow-y-auto">
+          {restaurants.slice(0, 5).map((restaurant, index) => (
+            <div key={index} className=" h-64 w-48 p-4 rounded-2xl ">
+              <img
+                src={`/images/${restaurant.image_url}`}
+                alt={`Image of ${restaurant.name}`}
+                className=" w-full h-40 object-cover rounded-xl cursor-pointer"
+                loading="lazy"
+              />
+              <p className="w-full text-center bg-Yellow-Green mt-2 p-1 rounded2xl hover:bg-Yellow-Green">
+                {restaurant.name}
+              </p>
+            </div>
+          ))}
+        </div>
       </div>
-    ))}
-  </div>
-</div>
 
-
-      <Footer/>
+      <Footer />
     </div>
   );
 }
-
