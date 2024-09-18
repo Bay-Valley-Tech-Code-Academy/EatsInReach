dbf8f8b003e5a74a641bae466d96a200e747f5f6
Misael Bolainez <misaelbolainez@gmail.com>
2024-09-18 09:57:26 -0700

    fix(page access): Added more checks for pages for certain roles cant access

--- src/app/Pages/UserProfile/page.js
+++ src/app/Pages/UserProfile/page.js
@@ -1,14 +1,79 @@
-"use client"
-import Link from 'next/link';
-import Navbar from '@/Components/Navbar'
+"use client";
+import Link from "next/link";
+import Navbar from "@/Components/Navbar";
+import { useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { useAuth } from "../../../../context/authContext";
+import { doc, getDoc } from "firebase/firestore";
+import { firestore } from "../../../../firebase";
 
-export default function LoginVendor() {
-    return(
-        <div>
-            <Navbar />
-            <div>User Profile</div>
-            <Link href="/">Home</Link>
-        </div>
-        
-    )
+export default function UserProfile() {
+  const router = useRouter();
+  const { currentUser, loading } = useAuth();
+  const [role, setRole] = useState(null);
+  const [isRoleLoading, setIsRoleLoading] = useState(true);
+
+  useEffect(() => {
+    // Redirect to the landing page if the user is not logged in
+    if (!loading && !currentUser) {
+      router.push("/");
+    }
+    if (currentUser) {
+      const fetchUserData = async () => {
+        const collections = ["users", "vendors", "admins"];
+        let found = false;
+
+        for (const collection of collections) {
+          if (found) break;
+
+          try {
+            const userDoc = await getDoc(
+              doc(firestore, collection, currentUser.uid)
+            );
+
+            if (userDoc.exists()) {
+              const userData = userDoc.data();
+              if (userData.role !== "user") {
+                router.push("/");
+              }
+              found = true;
+              setRole(userData.role);
+            }
+          } catch (error) {
+            console.error(
+              `Error fetching user data from ${collection} collection:`,
+              error
+            );
+          }
+        }
+
+        if (!found) {
+          console.log(
+            "User document does not exist in any of the collections."
+          );
+          setRole(null);
+        }
+        setIsRoleLoading(false);
+      };
+
+      fetchUserData();
+    }
+  }, [currentUser, loading, router]);
+
+  // Show a loading indicator or null while checking auth state
+  if (loading || isRoleLoading) {
+    return <div>Loading...</div>; // Replace with a loading spinner if needed
+  }
+
+  if (!currentUser || role !== "user") {
+    return <div>Redirecting...</div>;
+  }
+
+  return (
+    <div>
+      <Navbar />
+      <div>User Profile</div>
+      <Link href="/">Home</Link>
+    </div>
+  );
 }
