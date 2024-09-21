bd125bc01555891dcba210dc478cf0c76270c38c
Misael Bolainez <misaelbolainez@gmail.com>
2024-09-20 18:55:36 -0700

    feat(user settings): Users can now reset their email, password, and username

--- src/app/Pages/UserProfile/page.js
+++ src/app/Pages/UserProfile/page.js
@@ -3,9 +3,15 @@ import Navbar from "@/Components/Navbar";
 import { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
 import { useAuth } from "../../../../context/authContext";
-import { doc, getDoc } from "firebase/firestore";
-import { firestore } from "../../../../firebase";
-import { updateEmail, updatePassword } from "firebase/auth";
+import { doc, getDoc, updateDoc } from "firebase/firestore";
+import { auth, firestore } from "../../../../firebase";
+import {
+  verifyBeforeUpdateEmail,
+  updatePassword,
+  EmailAuthProvider,
+  reauthenticateWithCredential,
+  signOut,
+} from "firebase/auth";
 
 export default function UserProfile() {
   const router = useRouter();
@@ -16,8 +22,8 @@ export default function UserProfile() {
   const [newEmail, setNewEmail] = useState("");
   const [newPassword, setNewPassword] = useState("");
   const [currentPassword, setCurrentPassword] = useState("");
+  const [currentPasswordEmail, setCurrentPasswordEmail] = useState("");
   const [error, setError] = useState(null);
-  const [successMessage, setSuccessMessage] = useState(null);
 
   useEffect(() => {
     // Redirect to the landing page if the user is not logged in
@@ -87,20 +93,34 @@ export default function UserProfile() {
       await updateDoc(userDocRef, {
         userName: newUsername,
       });
-      setSuccessMessage("Username updated successfully!");
+      router.replace("/Pages/Restaurants");
     } catch (error) {
+      console.log(error);
       setError("Failed to update username: " + error.message);
     }
   };
 
   const handleEmailChange = async (e) => {
     e.preventDefault();
+
     try {
-      await reauthenticate(currentPassword);
-      await updateEmail(currentUser, newEmail);
-      setSuccessMessage("Email updated successfully!");
+      // Ensure reauthentication
+      await reauthenticate(currentPasswordEmail);
+      await verifyBeforeUpdateEmail(currentUser, newEmail);
+      await signOut(auth)
+        .then(() => console.log("User signed out"))
+        .catch((error) => console.error("Sign out error: ", error));
     } catch (error) {
-      setError("Failed to update email: " + error.message);
+      console.error(error);
+
+      // Handle specific Firebase errors
+      if (error.code === "auth/email-already-in-use") {
+        setError("This email is already in use. Please try a different one.");
+      } else if (error.code === "auth/operation-not-allowed") {
+        setError("This operation is not allowed.");
+      } else {
+        setError("Failed to update email: " + error.message);
+      }
     }
   };
 
@@ -109,7 +129,9 @@ export default function UserProfile() {
     try {
       await reauthenticate(currentPassword);
       await updatePassword(currentUser, newPassword);
-      setSuccessMessage("Password updated successfully!");
+      await signOut(auth)
+        .then(() => console.log("User signed out"))
+        .catch((error) => console.error("Sign out error: ", error));
     } catch (error) {
       setError("Failed to update password: " + error.message);
     }
@@ -131,31 +153,31 @@ export default function UserProfile() {
         <h1>Account Settings</h1>
       </div>
 
-      <div className="flex max-md:flex-col max-md:items-center md:h-[calc(100vh-100px)] bg-Almond justify-between overflow-hidden">
+      <div className="flex max-md:flex-col max-md:items-center md:h-[calc(100vh-100px)] bg-Almond overflow-hidden">
         {/* sidebar */}
-        <div className="bg-[#FDE4CE] drop-shadow-xl md:h-[calc(100vh-120px)] w-2/6 max-md:w-3/4 max-md:ml-1 md:ml-10 mt-3 rounded-lg m-2">
+        <div className="bg-[#FDE4CE] drop-shadow-xl md:h-[calc(100vh-120px)] w-2/6 md:w-1/12 max-md:w-3/4 max-md:ml-1 md:ml-10 mt-3 rounded-lg m-2">
           <ul>
             <li className="hover:bg-Yellow-Green active:bg-green-700">
-              <div className="flex ml-3 p-2">
+              <div className="flex flex-wrap justify-center p-2">
                 <img
                   src="/images/profile-icon.png"
                   height="15"
                   width="25"
                   alt="Profile"
                 />
-                <h1 className="ml-5">Profile</h1>
+                <h1 className=" text-center">Profile</h1>
               </div>
             </li>
           </ul>
         </div>
-        <div className=" bg-[#b5d773] flex flex-col h-[calc(100vh-130px)] w-full justify-between items-center md:p-1 md:m-5 md:ml-14 rounded-lg">
+        <div className=" bg-[#b5d773] flex flex-col h-[calc(100vh-130px)] w-4/6 max-md:w-3/4 justify-between items-center md:p-1 md:m-5 md:ml-28 rounded-lg">
           <div className="flex justify-self-start mt-5 underline">
             <h1>Profile Settings</h1>
           </div>
 
-          <div className=" bg-Dartmouth-green flex flex-col justify-center items-center space-y-10 h-full px-10 my-16 rounded-3xl">
+          <div className=" bg-Dartmouth-green flex flex-col justify-center items-center w-5/6 max-md:w-3/4 space-y-8 h-full my-16 rounded-3xl">
             <form
-              className="flex max-sm:flex-col max-sm:items-center"
+              className="flex max-lg:flex-col max-lg:items-center m-2"
               onSubmit={handleUsernameChange}
             >
               <h1 className="text-white">Change Username</h1>
@@ -170,48 +192,55 @@ export default function UserProfile() {
                 Change
               </button>
             </form>
-            <form className="flex max-sm:flex-col max-sm:items-center" onSubmit={handleEmailChange}>
+            <form
+              className="flex max-lg:flex-col max-lg:items-center m-2"
+              onSubmit={handleEmailChange}
+            >
               <h1 className="mr-2 text-white">Change Email</h1>
-              <input
-                type="email"
-                placeholder="Enter new email..."
-                className="outline-none mr-2 pl-2 m-1 rounded-md"
-                value={newEmail}
-                onChange={(e) => setNewEmail(e.target.value)}
-              />
-              <input
-                type="password"
-                placeholder="Enter current password..."
-                className="outline-none mr-2 pl-2 m-1 rounded-md"
-                value={currentPassword}
-                onChange={(e) => setCurrentPassword(e.target.value)}
-              />
-              <button className="bg-Almond outline-2 px-2 rounded-md">
-                Change
-              </button>
+              <div className="flex max-lg:flex-col">
+                <input
+                  type="email"
+                  placeholder="Enter new email..."
+                  className="outline-none mr-2 pl-2 m-1 rounded-md"
+                  value={newEmail}
+                  onChange={(e) => setNewEmail(e.target.value)}
+                />
+                <input
+                  type="password"
+                  placeholder="Enter current password..."
+                  className="outline-none mr-2 pl-2 m-1 rounded-md"
+                  value={currentPasswordEmail}
+                  onChange={(e) => setCurrentPasswordEmail(e.target.value)}
+                />
+                <button className="bg-Almond outline-2 px-2 rounded-md">
+                  Change
+                </button>
+              </div>
             </form>
             <form
-              className="flex max-sm:flex-col max-sm:items-center"
+              className="flex max-lg:flex-col max-lg:items-center m-2"
               onSubmit={handlePasswordChange}
             >
               <h1 className="text-white mr-2">Change Password</h1>
-              <input
-                type="password"
-                placeholder="Enter new password..."
-                className="outline-none mr-2 pl-2 m-1 rounded-md"
-                value={newPassword}
-                onChange={(e) => setNewPassword(e.target.value)}
-              />
-              <input
-                type="password"
-                placeholder="Enter current password..."
-                className="outline-none mr-2 pl-2 m-1 rounded-md"
-                value={currentPassword}
-                onChange={(e) => setCurrentPassword(e.target.value)}
-              />
-              <button className="bg-Almond outline-2 px-2 mb-1 rounded-md">
-                Change
-              </button>
+              <div className="flex max-lg:flex-col">
+                <input
+                  type="password"
+                  placeholder="Enter new password..."
+                  className="outline-none mr-2 pl-2 m-1 rounded-md"
+                  value={newPassword}
+                  onChange={(e) => setNewPassword(e.target.value)}
+                />
+                <input
+                  type="password"
+                  placeholder="Enter current password..."
+                  className="outline-none mr-2 pl-2 m-1 rounded-md"
+                  value={currentPassword}
+                  onChange={(e) => setCurrentPassword(e.target.value)}
+                />
+                <button className="bg-Almond outline-2 px-2 mb-1 rounded-md">
+                  Change
+                </button>
+              </div>
             </form>
           </div>
         </div>
