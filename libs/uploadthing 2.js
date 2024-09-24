3824376d3716d224d3428a4a50bfc8bd9d1056c0
JasonSYuen <js99168@icloud.com>
2024-09-24 15:33:46 -0700

    a

--- libs/uploadthing 2.js
+++
@@ -1,5 +0,0 @@
-import { generateUploadButton, generateUploadDropzone } from "@uploadthing/react";
-
-// No need for type annotations in JavaScript
-export const UploadButton = generateUploadButton();
-export const UploadDropzone = generateUploadDropzone();
