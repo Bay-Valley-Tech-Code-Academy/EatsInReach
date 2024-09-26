853c2365b79118b2a2b870523b9c5013c9b7922f
Kimberly Encinas <kimbencinas@gmail.com>
2024-09-25 22:46:11 -0700

    Finalize responsiveness for contact page

--- src/app/Pages/Contact/page.js
+++ src/app/Pages/Contact/page.js
@@ -39,14 +39,14 @@ export default function Contact() {
     return(
         <div className="bg-Yellow-Green min-h-screen">
             <Navbar />
-            <div className="flex items-center justify-center gap-6">
-                <div>
-                    <p>Contact Us</p>
-                    <p>Fill out the form and our term will get back to you as soon as possible.</p>
-                    <p>EatsInReach@gmail.com</p>
+            <div className="flex flex-col xs:flex-col md:flex-row items-center items-center justify-center xs:gap-10 lg:gap-32 md:p-6">
+                <div className="xs:text-center md:text-left">
+                    <p className="font-semibold text-3xl">Contact Us</p>
+                    <p className="mt-4">Fill out the form and our term will get back to you as soon as possible.</p>
+                    <p className="mt-1">EatsInReach@gmail.com</p>
                 </div>
-                <div className="bg-gray-100 mt-20 p-10 rounded-xl w-1/2">
-                    <h3 className="text-center font-semibold text-xl">Get in Touch</h3>
+                <div className="bg-gray-100 xs:mt-0 md:mt-20 lg:mt-20 p-10 rounded-xl xs:w-2/3 md:w-1/2 lg:w-1/3">
+                    <h3 className="text-center font-semibold text-2xl mb-2">Get in Touch</h3>
                     {submitForm ? ( 
                         <p>Message sent successfully.</p>
                     ) : (
@@ -58,7 +58,7 @@ export default function Contact() {
                                     name="name"
                                     value={formData.name}
                                     onChange={handleChange}
-                                    className="w-full"
+                                    className="w-full mt-1 outline outline-1 outline-gray-400 rounded-sm"
                                     required
                                 />
                             </div>
@@ -66,7 +66,7 @@ export default function Contact() {
                                 <label className="block">Email</label>
                                 <input
                                     type="email"
-                                    className="w-full"
+                                    className="w-full mt-1 outline outline-1 outline-gray-400 rounded-sm"
                                     name="email"
                                     value={formData.email}
                                     onChange={handleChange}
@@ -79,11 +79,11 @@ export default function Contact() {
                                     name="message"
                                     value={formData.message}
                                     onChange={handleChange}
-                                    className="w-full"
+                                    className="w-full mt-1 outline outline-1 outline-gray-400 rounded-sm"
                                     required
                                 ></textarea>
                             </div>
-                            <button type="submit" className="bg-green-600 p-1 align-center w-full">Send Message</button>
+                            <button type="submit" className="bg-black text-white rounded-md p-1 mt-1 align-center w-full">Submit</button>
                             {error && <p>{error}</p>}
                         </form>
                     )}
